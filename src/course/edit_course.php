1b8e05babb550bf2401291540dcca56d5f601dee
Paxius025 <pantongbk@gmail.com>
2025-09-19 21:00:36 +0700

    Prevent duplicate course code/name before saving to database

--- src/course/edit_course.php
+++ src/course/edit_course.php
@@ -4,16 +4,26 @@ $course_code = $_GET["course_code"];
 $sql = "SELECT * FROM courses WHERE course_code='$course_code'";
 $result = mysqli_query($conn, $sql);
 $course = mysqli_fetch_assoc($result);
-echo "<center>";
-echo "<form action=save_course.php method=post>";
-echo "<table border=1 width=40%>";
-echo "<input type=hidden name=course_code_edit value=$course_code />";
-echo "<tr><td >Course Code:</td><td><input style=\"width: 200px;\" type=text name=course_code value=".$course["course_code"]."
-/></td></tr>";
-echo "<tr><td>Course Name:</td><td><input style=\"width: 200px;\" type=text name=course_name value='".$course["course_name"]."' /></td></tr>";
-echo "<tr><td>Credit:</td><td><input style=\"width: 200px;\" type=text name=credit value=".$course["credit"]." /></td></tr>";
-echo "<tr><td colspan=2><center><input type=submit value=Submit /></center></td></tr>";
-echo "</table>";
-echo "</form>";
-echo "</center>";
 ?>
+<form action="save_course.php" method="post">
+    <input type="hidden" name="course_code_edit" value="<?php echo htmlspecialchars($course_code); ?>" />
+    <table border="1">
+        <tr>
+            <td>Course Code:</td>
+            <td><input type="text" name="course_code" value="<?php echo htmlspecialchars($course["course_code"]); ?>" />
+            </td>
+        </tr>
+        <tr>
+            <td>Course Name:</td>
+            <td><input type="text" name="course_name" value="<?php echo htmlspecialchars($course["course_name"]); ?>" />
+            </td>
+        </tr>
+        <tr>
+            <td>Credit:</td>
+            <td><input type="text" name="credit" value="<?php echo htmlspecialchars($course["credit"]); ?>" /></td>
+        </tr>
+        <tr>
+            <td colspan="2"><input type="submit" value="Submit" /></td>
+        </tr>
+    </table>
+</form>
